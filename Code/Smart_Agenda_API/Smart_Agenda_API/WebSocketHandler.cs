1178317ca3d7eb70d0f125f32d659af0134bb751
Meefish <117977321+Meefish@users.noreply.github.com>
2023-12-26 18:58:23 +0100

    Fixed WebSocket polling problem
    The websocket doesn't poll now. Because there is always a connection the database will be fried by queries for LiveTaskEvents. To fix this the polling can be allowed by changing the comment

--- Code/Smart_Agenda_API/Smart_Agenda_API/WebSocketHandler.cs
+++ Code/Smart_Agenda_API/Smart_Agenda_API/WebSocketHandler.cs
@@ -16,42 +16,84 @@ namespace Smart_Agenda_API
             _scopeFactory = scopeFactory;
         }
 
+
+
         public async Task HandleWebSocketConnection(WebSocket webSocket)
         {
             var buffer = new byte[1024 * 4];
-            while (webSocket.State == WebSocketState.Open)
+            var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            if (result.MessageType == WebSocketMessageType.Text)
             {
-                var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-                if (result.MessageType == WebSocketMessageType.Text)
+                var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                var messageData = JsonConvert.DeserializeObject<Dictionary<string, string>>(message);
+                if (messageData.TryGetValue("calendarId", out var calendarIdString) && int.TryParse(calendarIdString, out var calendarId))
                 {
-                    var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
-                    var messageData = JsonConvert.DeserializeObject<Dictionary<string, string>>(message);
-                    if (messageData.TryGetValue("calendarId", out var calendarIdString) && int.TryParse(calendarIdString, out var calendarId))
-                    {
-                        if (!_sockets.ContainsKey(calendarIdString))
-                        {
-                            _sockets.Add(calendarIdString, webSocket);
-                        }
-
-                        using var scope = _scopeFactory.CreateScope();
-                        var calendarDAL = scope.ServiceProvider.GetRequiredService<ICalendarDAL>();
-                        var tasks = await calendarDAL.CheckTaskEvent(calendarId);
-
-                        var response = JsonConvert.SerializeObject(tasks);
-                        var responseBuffer = Encoding.UTF8.GetBytes(response);
-                        await webSocket.SendAsync(new ArraySegment<byte>(responseBuffer), WebSocketMessageType.Text, true, CancellationToken.None);
-                    }
+                    RegisterCalendarIdSocket(calendarId.ToString(), webSocket);
+                    await SendPushNotification(calendarId, webSocket);
                 }
             }
-
         }
 
-        public void SendPushNotification(string calendarIdString, string message)
+        private void RegisterCalendarIdSocket(string calendarIdString, WebSocket webSocket)
         {
-            if (_sockets.TryGetValue(calendarIdString, out var webSocket))
+            if (!_sockets.ContainsKey(calendarIdString))
+            {
+                _sockets.Add(calendarIdString, webSocket);
+            }
+            else
+            {
+
+                if (_sockets[calendarIdString] != webSocket)
+                {
+                    _sockets[calendarIdString] = webSocket;
+                }
+
+
+            }
+        }
+        private async Task SendMessageAsync(WebSocket webSocket, string message)
+        {
+            if (webSocket.State == WebSocketState.Open)
             {
                 var responseBuffer = Encoding.UTF8.GetBytes(message);
-                webSocket.SendAsync(new ArraySegment<byte>(responseBuffer), WebSocketMessageType.Text, true, CancellationToken.None);
+                await webSocket.SendAsync(new ArraySegment<byte>(responseBuffer), WebSocketMessageType.Text, true, CancellationToken.None);
+            }
+        }
+
+
+        private async Task SendPushNotification(int calendarId, WebSocket webSocket)
+        {
+            try
+            {
+                while (webSocket.State == WebSocketState.Open)
+                {
+                    using var scope = _scopeFactory.CreateScope();
+                    var calendarDAL = scope.ServiceProvider.GetRequiredService<ICalendarDAL>();
+                    var tasks = await calendarDAL.CheckTaskEvent(calendarId);
+                    if (tasks.Any())
+                    {
+                        var response = JsonConvert.SerializeObject(tasks);
+                        await SendMessageAsync(webSocket, response);
+                    }
+
+                    //    await Task.Delay(TimeSpan.FromSeconds(10));        //If polling would be allowed
+                }
+            }
+            finally
+            {
+                await CloseWebSocketAsync(calendarId.ToString(), webSocket);
+            }
+        }
+        private async Task CloseWebSocketAsync(string calendarIdString, WebSocket webSocket)
+        {
+            if (_sockets.ContainsKey(calendarIdString))
+            {
+                _sockets.Remove(calendarIdString);
+            }
+
+            if (webSocket.State != WebSocketState.Closed && webSocket.State != WebSocketState.Aborted)
+            {
+                await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
             }
         }
     }
